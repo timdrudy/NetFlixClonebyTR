5c2ae77065f8539ffdeab4c5804d9dd96ed0eea2
Tim <timdrudy@gmail.com>
2025-04-09 14:20:58 -0500

    completed netflix clone

---
+++ src/firebase.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import { 
+    createUserWithEmailAndPassword, 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    signOut} from "firebase/auth";
+import { addDoc, 
+    collection, 
+    getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4e1BtBZG64jW9PuMThP1bsSPPTl833DA",
+  authDomain: "netflix-clone-by-tr.firebaseapp.com",
+  projectId: "netflix-clone-by-tr",
+  storageBucket: "netflix-clone-by-tr.firebasestorage.app",
+  messagingSenderId: "401825492098",
+  appId: "1:401825492098:web:4ecc5c763bcdb9c79d989b"
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email, password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = ()=>{
+    signOut(auth);
+} 
+
+export {auth, db, login, signup, logout};
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Player.css";
 import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
+
+const {id} = useParams();
+const navigate = useNavigate();
 
   const [apiData, setApiData] = useState({
     name: "",
@@ -22,7 +26,7 @@ const Player = () => {
 
   useEffect(()=>{
     fetch(
-      "https://api.themoviedb.org/3/movie/950387/videos?language=en-US",
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
       options
     )
       .then((res) => res.json())
@@ -34,19 +38,19 @@ const Player = () => {
 
   return (
     <div className="player">
-      <img src={back_arrow_icon} alt="" />
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}} />
       <iframe
         width="90%"
         height="90%"
-        src="https://www.youtube.com/embed/If_UO5D9SdU"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
         title="trailer"
         frameBorder="0"
         allowFullScreen
       ></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   );
